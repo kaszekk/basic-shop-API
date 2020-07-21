48fd332564151d744c16f09e049908b55fc2d99d
Lukasz <lukmyrta@gmail.com>
2020-07-21 10:06:16 +0200

    model classes defined

---
+++ src/main/java/Order.java
@@ -0,0 +1,35 @@
+import io.swagger.annotations.ApiModelProperty;
+import java.time.LocalDate;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Order {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @ApiModelProperty(value = "Order id.", dataType = "Long", position = -1)
+  private Long id;
+
+  @ApiModelProperty(value = "Short order description.", example = "Pizza", dataType = "String")
+  private String description;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  private Customer customer;
+
+  @ApiModelProperty(value = "date of order.", example = "21-07-2020", dataType = "LocalDate")
+  private LocalDate date;
+
+}
