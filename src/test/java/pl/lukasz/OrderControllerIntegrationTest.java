5854097f3470268d2ab6c36c5208b414202d79df
Lukasz <lukmyrta@gmail.com>
2020-07-22 18:16:19 +0200

    getAllOrders tests added

--- src/test/java/pl/lukasz/OrderControllerIntegrationTest.java
+++ src/test/java/pl/lukasz/OrderControllerIntegrationTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.time.LocalDate;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -22,7 +23,7 @@ import pl.lukasz.model.Order;
 class OrderControllerIntegrationTest extends IntegrationTestBase {
 
   @Test
-  public void shouldReturnIdWhenEmployeeAddedSuccessfully() throws Exception {
+  public void shouldReturnIdWhenOrderAddedSuccessfully() throws Exception {
     // given
     Buyer buyer = new Buyer("Franek", "Kimono");
     LocalDate orderDate = LocalDate.of(2020, 7, 20);
@@ -53,6 +54,56 @@ class OrderControllerIntegrationTest extends IntegrationTestBase {
     // then
     assertThat(numberOfAllOrdersInDb, is(equalTo(1)));
     assertThat(addedOrder, is(equalTo(expectedOrder)));
+  }
+
+  @Test
+  public void shouldGetAllOrders() throws Exception {
+    // given
+    LocalDate pizzaOrderDate = LocalDate.of(2020, 6, 1);
+    LocalDate spriteOrderDate = LocalDate.of(2020, 6, 10);
+    LocalDate bazookaOrderDate = LocalDate.of(2020, 4, 11);
+    LocalDate chipsOrderDate = LocalDate.of(2020, 5, 13);
+
+    Buyer pizzaBuyer = new Buyer("Franek", "Kimono");
+    Buyer spriteBuyer = new Buyer("Ferdynand", "Kiepski");
+    Buyer bazookaBuyer = new Buyer("Zulu", "Gula");
+    Buyer chipsBuyer = new Buyer("Justin", "Bieber");
+
+    Order pizzaOrder = new Order("Pizza", pizzaBuyer, pizzaOrderDate);
+    Order spriteOrder = new Order("Sprite", spriteBuyer, spriteOrderDate);
+    Order bazookaOrder = new Order("Bazooka", bazookaBuyer, bazookaOrderDate);
+    Order chipsOrder = new Order("Chips", chipsBuyer, chipsOrderDate);
+
+    final long pizzaOrderId = callRestToAddOrderAndReturnId(pizzaOrder);
+    final long spriteOrderId = callRestToAddOrderAndReturnId(spriteOrder);
+    final long bazookaOrderId = callRestToAddOrderAndReturnId(bazookaOrder);
+    final long chipsOrderId = callRestToAddOrderAndReturnId(chipsOrder);
+
+    assignIdsToOrderInstances(
+        pizzaOrder, pizzaOrderId,
+        spriteOrder, spriteOrderId,
+        bazookaOrder, bazookaOrderId,
+        chipsOrder, chipsOrderId
+    );
+
+    // when
+    final List<Order> allOrdersFromDb = callRestToGetAllOrders();
+
+    // then
+    assertThat(allOrdersFromDb.size(), is(equalTo(4)));
+
+    assertThat(allOrdersFromDb.get(0), is(equalTo(pizzaOrder)));
+    assertThat(allOrdersFromDb.get(1), is(equalTo(spriteOrder)));
+    assertThat(allOrdersFromDb.get(2), is(equalTo(bazookaOrder)));
+    assertThat(allOrdersFromDb.get(3), is(equalTo(chipsOrder)));
+  }
+
+  private void assignIdsToOrderInstances(Order pizzaOrder, long pizzaOrderId, Order spriteOrder, long spriteOrderId, Order bazookaOrder,
+      long bazookaOrderId, Order chipsOrder, long chipsOrderId) {
+    pizzaOrder.setId(pizzaOrderId);
+    spriteOrder.setId(spriteOrderId);
+    bazookaOrder.setId(bazookaOrderId);
+    chipsOrder.setId(chipsOrderId);
   }
 
 }
